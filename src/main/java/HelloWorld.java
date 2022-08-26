693ae90c61188ad2b864ff09a9fbe93edfe1c73e
shyding <1058926697@qq.com>
2022-08-26 16:47:40 +0800

    添加实际可以运行的demo

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,11 +1,15 @@
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.MarkerManager;
 
 
 public class HelloWorld {
 	private static final Logger logger = LogManager.getLogger("HelloWorld");
 
 	public static void main(String[] args) {
-		logger.info("Hello, World!");
+		Marker marker = MarkerManager.getMarker("dsy");
+		logger.info(marker , "Hello, World!");
+		logger.info( "test222!");
 	}
 }
